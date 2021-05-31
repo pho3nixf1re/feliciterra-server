27d01d6a0c873e52c17fce10064e6c52392a9c51
Matthew Turney <matthew.a.turney@gmail.com>
2021-05-30 20:36:41 -0500

    Update publish script to work from project root

--- openvpn/publish.sh
+++ openvpn/publish.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
-docker build openvpn --file ./Dockerfile --tag docker.pkg.github.com/pho3nixf1re/feliciterra-server/openvpn:latest
+docker build openvpn --file ./openvpn/Dockerfile --tag docker.pkg.github.com/pho3nixf1re/feliciterra-server/openvpn:latest
 docker push docker.pkg.github.com/pho3nixf1re/feliciterra-server/openvpn:latest
