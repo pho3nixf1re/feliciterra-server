bc44d651fdea8fde8f0a46c6bdc4652eaec8c253
Matthew Turney <matthew.a.turney@gmail.com>
2021-12-22 22:14:00 -0600

    remove old openvpn dockerfile

--- openvpn/publish.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-
-docker build openvpn --file ./openvpn/Dockerfile --tag docker.pkg.github.com/pho3nixf1re/feliciterra-server/openvpn:latest
-docker push docker.pkg.github.com/pho3nixf1re/feliciterra-server/openvpn:latest
